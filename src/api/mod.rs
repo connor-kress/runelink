abd623479b1e0326c6ea97955ebcb40edb309774
Connor Kress <ckress@ufl.edu>
2025-05-04 22:25:19 -0400

    axum query params practice

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,18 +1,21 @@
-use axum::response::IntoResponse;
-// use serde::Deserialize;
+use axum::{extract::Query, response::IntoResponse};
+use serde::Deserialize;
 
-// #[derive(Deserialize)]
-// pub struct LoginRequest {
-//     pub username: String,
-//     pub password: String,
-// }
+#[derive(Deserialize)]
+pub struct PingParams {
+    id: Option<i32>,
+    msg: Option<String>,
+}
 
-// pub async fn login(Json(_payload): Json<LoginRequest>) -> impl IntoResponse {
-//     // Authenticate user, return JWT or error
-//     return "OK";
-// }
-
-pub async fn ping() -> impl IntoResponse {
-    println!("pong");
-    return "pong";
+pub async fn ping(Query(params): Query<PingParams>) -> impl IntoResponse {
+    let user_msg = match params.msg {
+        Some(msg) => format!("\"{}\"", msg),
+        None => "No message".to_owned(),
+    };
+    let message = match params.id {
+        Some(id) => format!("pong ({}): {}", id, user_msg),
+        None => format!("pong: {}", user_msg),
+    };
+    println!("{}", message);
+    return message;
 }
